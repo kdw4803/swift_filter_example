f4b91dd1cbc1c6f85144beda820f3b3e3efeffa5
Brad <brad@Bradui-MacBook-Pro.local>
2017-03-22 06:47:13 +0900

    first commit

---
+++ instagram-clone/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  instagram-clone
+//
+//  Created by Brad on 20/03/2017.
+//  Copyright © 2017 brad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.allowsEditing = false
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
